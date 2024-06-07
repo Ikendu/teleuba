d92bda5780f93d2ad448c2567dd0f79e59beef16
Aniede Chibundu <ndubest56@gmail.com>
2024-06-07 10:53:21 +0100

    added config for secure https

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync("./certs/cert.key"),
+      cert: fs.readFileSync("./certs/cert.crt"),
+    },
+  },
   plugins: [react()],
-})
+});
