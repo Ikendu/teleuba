767d9f949acdba4d95416aefafd7c80de5677403
Aniede Chibundu <ndubest56@gmail.com>
2024-06-07 13:07:34 +0100

    more on token sign and verify

--- server/expressRoute.js
+++ server/expressRoute.js
@@ -1,6 +1,31 @@
 // this is where all our express routes and related stuffs are done
 const app = require("./server").app;
+const jwt = require("jsonwebtoken");
 
-app.get("/test", (req, res) => {
-  res.json("Testing of app");
+const linkSecret = "fjduir3940@%kdfj,mxnruro";
+
+// this route is for us: in Production
+// would send this out, we will print it out and pest it in, It will drop
+// us in our React site with the right info for client to make an offer
+app.get("/user-link", (req, res) => {
+  // data for the end-user's appt
+  const apptData = {
+    proffesionalFullName: "Mr. Chibundu Aniede",
+    data: Date.now(),
+  };
+
+  // We need to encode this data in a token
+  const token = jwt.sign(apptData, linkSecret);
+
+  // res.send(`https://localhost:5173/join-video?token=${token}`);
+  res.send(token);
+
+  // res.json("Testing of app");
+});
+
+app.get("/validate-link", (req, res) => {
+  const { token } = req.query; //get the token from link request
+  const decodedToken = jwt.verify(token, linkSecret);
+
+  res.json(decodedToken);
 });
